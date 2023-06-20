0db5d0549c296176fd603dc2d90b4f7327f2d188
mss-souloftware <sufyan.souloftware@gmail.com>
2023-06-20 11:57:37 +0500

    files setup

---
+++ app/components/Header/SideBar.jsx
@@ -0,0 +1,15 @@
+
+export default function SideBar() {
+    return (
+        <header>
+            <div className="sideWrap fixed left-0 top-0 w-[5rem] h-full flex justify-center align-middle">
+                <button>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <small className="text-[14px] text-[#171918]">Menu</small>
+                </button>
+            </div>
+        </header >
+    )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
+import Container from './components/Container/Container'
+import SideBar from './components/Header/SideBar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ 
+  weight: ['200','400','600','800'],
+  subsets: ['latin'],
+ })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <SideBar />
+        <main id='main_Wrap' className='pl-20 pt-16'>
+          <Container>
+            {children}
+          </Container>
+        </main>
+      </body>
     </html>
   )
 }
